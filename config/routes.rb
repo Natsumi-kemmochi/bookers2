24ebf745ed003cb1d854dda36d67b8533ba83ff7
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-07-25 12:18:34 +0000

    [bookers2]ユーザーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :books, only: [:index, :new, :create, :show, :destroy, :edit, :update]
   devise_for :users
   #get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
+  resources :books, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  resources :users, only: [:show, :edit]
 end
