57027ff572accc3bd0448a7f7cc35c4b89259e90
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-07-26 15:02:58 +0000

    [bookers2]新規登録・ログインごとにフラッシュメッセージを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    if current_user
+      flash[:sing_up] = "Welcome! You have signed up successfully."
+      user_path(current_user)
+    else
+      flash[:sign_in] = "Signed in successfully." 
+      user_path(current_user)
+    end
+  end
+  
+  
   protected
-
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
